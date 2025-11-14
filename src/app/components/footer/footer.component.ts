a3429ab49e0de56bf045832e8c77697311c7a5ef
Vaishnavi Itape <156821431+VaishnaviItape@users.noreply.github.com>
2025-11-14 14:54:57 +0530

    Done the chnages as well removed the unwanted code

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, Inject } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
@@ -8,5 +10,22 @@ import { Component } from '@angular/core';
   styleUrl: './footer.component.css'
 })
 export class FooterComponent {
+ constructor(
+    @Inject(DOCUMENT) private document: Document,
+    private router: Router
+  ) {}
 
+  scrollTo(sectionId: string) {
+    // Update the fragment in URL
+    this.router.navigate([], {
+      fragment: sectionId,
+      queryParamsHandling: 'preserve'
+    });
+
+    // Smooth scroll
+    const element = this.document.getElementById(sectionId);
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }
+  }
 }
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,7 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
-
+import { Router, RouterModule } from '@angular/router';
+import {Inject } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 @Component({
   selector: 'app-header',
   standalone: true,
@@ -11,9 +12,32 @@ import { RouterModule } from '@angular/router';
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
-isMenuOpen = false;
+  isMenuOpen = false;
 
   toggleMenu() {
     this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  constructor(
+    @Inject(DOCUMENT) private document: Document,
+    private router: Router
+  ) {}
+
+
+  scrollTo(sectionId: string) {
+
+    // Update URL fragment without reloading the page
+    this.router.navigate([], {
+      fragment: sectionId,
+      queryParamsHandling: 'preserve'
+    });
+
+    // Smooth scroll
+    const element = this.document.getElementById(sectionId);
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }
+
+    this.isMenuOpen = false;
   }
 }
