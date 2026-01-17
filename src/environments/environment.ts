e69ff809c23e8b6cac63b055b97877cb39f47ff6
Vaishnavi Itape <156821431+VaishnaviItape@users.noreply.github.com>
2026-01-17 14:07:09 +0530

    Added the project details

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,13 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: "AIzaSyDsuAbPV7DKvA2JzK4x-5egiT3oR6wdWM4",
-    authDomain: "qfreemartlandigpage.firebaseapp.com",
-    databaseURL: "https://qfreemartlandigpage-default-rtdb.firebaseio.com",
-    projectId: "qfreemartlandigpage",
-    storageBucket: "qfreemartlandigpage.firebasestorage.app",
-    messagingSenderId: "564523759514",
-    appId: "1:564523759514:web:9fa3a25ddc490973eeec23"
+    apiKey: "AIzaSyBO7fyxtpvgagzz_7uEbYUrA6Fat9sdzZo",
+    authDomain: "qfreemart-testing.firebaseapp.com",
+    databaseURL: "https://qfreemart-testing-default-rtdb.firebaseio.com",
+    projectId: "qfreemart-testing",
+    storageBucket: "qfreemart-testing.firebasestorage.app",
+    messagingSenderId: "1014725496384",
+    appId: "1:1014725496384:web:68265f74f2cc09293559a5",
+    measurementId: "G-PL67R7GESK"
   }
 };
